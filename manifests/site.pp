83591f3915f18a1a578dcb9f2a69bbbb8fc81a2d
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-11-23 01:31:07 +0100

    Initial commit, add all servers

---
+++ manifests/site.pp
@@ -0,0 +1,34 @@
+node vader {
+}
+
+node sidious {
+}
+
+node nihilus {
+}
+
+node angral {
+}
+
+node karnage {
+}
+
+node tyranus {
+}
+
+node vectivus {
+}
+
+node voren {
+}
+
+node vectivus {
+}
+
+node voren {
+}
+
+node zash {
+}
+
+# vim: set autoindent softtabstop=2 expandtab textwidth=80 shiftwidth=2:
