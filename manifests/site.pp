cb44ea94a01a8ff7aafb19434ea1948d18c240a6
Theo Chatzimichos <tampakrap@linuxteam.teilar.gr>
2012-11-24 20:57:01 +0100

    remove the notify, since the hosts work fine now

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,7 +14,6 @@ node tyranus {
 }
 
 node vader {
-  include base
 }
 
 node vectivus {
@@ -24,7 +23,6 @@ node voren {
 }
 
 node zash {
-  include base
 }
 
 # vim: set autoindent softtabstop=2 expandtab textwidth=80 shiftwidth=2:
--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,5 +1,4 @@
 class base {
-  notify{"my hostname is ${hostname}" :}
 }
 
 # vim: set autoindent softtabstop=2 expandtab textwidth=80 shiftwidth=2:
